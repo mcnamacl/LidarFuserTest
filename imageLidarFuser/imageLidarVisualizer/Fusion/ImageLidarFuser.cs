3d38881af3d98f2f681ef136952873dba7fb0c85
mcnamacl <mcnamacl@tcd.ie>
2019-03-29 13:42:28 +0000

    left and right cones now work

--- imageLidarFuser/imageLidarVisualizer/Fusion/ImageLidarFuser.cs
+++ imageLidarFuser/imageLidarVisualizer/Fusion/ImageLidarFuser.cs
@@ -29,42 +29,16 @@ namespace imageLidarVisualizer.Fusion
 
             
             ObstacleMap obstacleMap = new ObstacleMap();
-            // TODO - CategorizeObstacles - create obstacles and insert into left and right road
-            // -> Add them to obstacleMap.LeftObstacles or obstacleMap.RightObstacles
-            // -> You can use Computer Vision here
-            // -> Think of useful heuristics for determining the road.
 
             
             //orientate the obstacles
             List<PredictedObstacle> rotatedObstacles = orientate(predictedObstacles, Input.State);
-            //rotatedObstacles = rotatedObstacles.Where(O => O.MeanY > 0).ToList();
-
-            //HashSet<int> idSet = new HashSet<int>();
-            //for (int i = 0; i < rotatedObstacles.Count(); i++)
-            //{
-            //    idSet.Add(rotatedObstacles[i].ID);
-            //}
-
-            //List<PredictedObstacle> newPredictedObs = new List<PredictedObstacle>();
-
-            //for (int i = 0; i < predictedObstacles.Count(); i++)
-            //{
-            //    if (idSet.Contains(predictedObstacles[i].ID))
-            //    {
-            //        newPredictedObs.Add(predictedObstacles[i]);
-            //    }
-            //}
 
             rotatedObstacles = rotatedObstacles.OrderBy((O) => O.MeanY).ToList();
 
-            //assign obstacles left and right
-            obstacleMap = sortLeftAndRight(rotatedObstacles, predictedObstacles);
+            obstacleMap = designateForStraight(obstacleMap, rotatedObstacles, predictedObstacles);
 
             Mat threshHoldImage = GetThresholdImageCone(Input.Image);
-
-            // Maybe TODO map ran through odometry to check for missed obstacles
-            //ObstacleMap ReturnedMap = AddOdometricEstimation(obstacleMap);
-            //InitialState = Input.State;
 
             return obstacleMap;
         }
@@ -92,125 +66,44 @@ namespace imageLidarVisualizer.Fusion
             return (x * (float)Math.Sin(theta)) + (y * (float)Math.Cos(theta));
         }
 
-        private ObstacleMap sortLeftAndRight(List<PredictedObstacle> obs, List<PredictedObstacle> original)
-        {
-            ObstacleMap map = new ObstacleMap();
-
-            PredictedObstacle maxY = obs.LastOrDefault();
-            PredictedObstacle minYLeft = obs.LastOrDefault();
-            PredictedObstacle minYRight = obs.LastOrDefault();
-
-
-            bool foundRight = false, foundLeft = false;
-
-            foreach (PredictedObstacle obj in obs)
-            {
-                if (obj.MeanX > 0 && !foundRight)
-                {
-                    minYRight = obj;
-                    foundRight = true;
-                }
-                else if (obj.MeanX < 0 && !foundLeft){
-                    minYLeft = obj;
-                    foundLeft = true;
-                }
-                if (foundLeft && foundRight)
-                {
-                    break;
-                }
-            }
-
-
-            Side pos = Side.Right; //left -> true, right -> false
-
-            if (maxY.MeanX < minYLeft.MeanX && Math.Abs(maxY.MeanX - minYLeft.MeanX) > 3)
-            {
-                pos = Side.Left; //left turn
-                map = designateForTurn(pos, map, obs, minYRight, maxY, original);
-            } 
-            else if (maxY.MeanX > minYRight.MeanX && Math.Abs(maxY.MeanX - minYRight.MeanX) > 3)
-            {
-                //right turn
-                MessageBox.Show(maxY.MeanX.ToString() + " " + maxY.MeanY.ToString() + " " + minYLeft.MeanX.ToString() + " " + minYLeft.MeanY.ToString() + " " + minYLeft.ID);
-
-                map = designateForTurn(pos, map, obs, minYLeft, maxY, original);
-            } else
-            {
-                map = designateForStraight(map, obs, original);
-            }
-            
-            return map;
-        }
-
         private ObstacleMap designateForStraight(ObstacleMap map, List<PredictedObstacle> obs, List<PredictedObstacle> original)
-        {
-            for (int i = 0; i < obs.Count(); i++)
-            {
-                Obstacle newObs = new Obstacle(original[i].MeanX, original[i].MeanY, 1f);
-                if (obs[i].MeanX > 0)
-                {
-                    map.ObstaclesRight.AddLast(newObs);
-                } else
-                {
-                    map.ObstaclesLeft.AddLast(newObs);
-                }
-            }
-            return map;
-        }
-
-        private ObstacleMap designateForTurn(Side pos, ObstacleMap map, List<PredictedObstacle> obs, PredictedObstacle p0, PredictedObstacle p1, List<PredictedObstacle> original)
         {
             obs = obs.OrderBy((O) => O.ID).ToList();
             original = original.OrderBy((O) => O.ID).ToList();
-            for (int i = 0; i < obs.Count(); i++) 
+            List<Obstacle> right = new List<Obstacle>();
+            List<Obstacle> left = new List<Obstacle>();
+
+            //assign left and right
+            for (int i = 0; i < obs.Count(); i++)
             {
                 Obstacle newObs = new Obstacle(original[i].MeanX, original[i].MeanY, 1f);
-                Side side = line(p0, p1, obs[i]);
-                if (side == Side.Right) // righthand side
+                if (obs[i].MeanY > 0)
                 {
-                    map.ObstaclesRight.AddLast(newObs);
-                    //MessageBox.Show("Right " + obs[i].MeanX.ToString() + " " + obs[i].MeanY.ToString() + " " + obs[i].ID);
-                } 
-                else if (side == Side.Left) // lefthand side
-                {
-                    map.ObstaclesLeft.AddLast(newObs);
-                    //MessageBox.Show("Left " + obs[i].MeanX.ToString() + " " + obs[i].MeanY.ToString() + " " + obs[i].ID);
-                }
-                else if (side == Side.Centre && pos == Side.Left)
-                {
-                    //MessageBox.Show("Right " + obs[i].MeanX.ToString() + " " + obs[i].MeanY.ToString() + " " + obs[i].ID);
-                    map.ObstaclesLeft.AddLast(newObs);
-                }
-                else if (side == Side.Centre && pos == Side.Right)
-                {
-                    //MessageBox.Show("Left " + obs[i].MeanX.ToString() + " " + obs[i].MeanY.ToString() + " " + obs[i].ID);
-                    map.ObstaclesRight.AddLast(newObs);
+                    if (obs[i].MeanX > 0)
+                    {
+                        right.Add(newObs);
+                    }
+                    else
+                    {
+                        left.Add(newObs);
+                    }
                 }
             }
-            return map;
-        }
 
-        //p0=min p0x = x1, p0y = y1, p1=max p1x = x2, p1y = y2, p2=point DO NOT KNOW IF POINT IS TO THE LEFT OR  RIGHT OF THE LINE 
+            right = right.OrderBy((O) => O.Y).ToList();
+            left = left.OrderBy((O) => O.Y).ToList();
 
-        private Side line(PredictedObstacle p0, PredictedObstacle p1, PredictedObstacle p2)
-        {
-            double val = (p2.MeanX - p0.MeanX) * (p1.MeanY - p0.MeanY) - (p2.MeanY - p0.MeanY) * (p1.MeanX - p0.MeanX);
-            if (val < 0.1 && val > -0.1)
+            foreach (Obstacle obj in right)
             {
-                return Side.Centre;
+                map.ObstaclesRight.AddLast(obj);
             }
-            return (val > 0) ? Side.Right : Side.Left;
-        }
 
-        private ObstacleMap AddOdometricEstimation(ObstacleMap obstacleMap)
-        {
-            int CurrentStamp = TimeUtils.GetTimeStamp();
-            ObstacleMap EstimatedMap = OdometricEstimator.Run(obstacleMap, InitialStamp, CurrentStamp, InitialState);
+            foreach (Obstacle obj in left)
+            {
+                map.ObstaclesLeft.AddLast(obj);
+            }
 
-            // TODO - reference predicted obstacles against actual new obstacles
-            // to check if the old reading has obstacles that the new one has missed
-
-            return EstimatedMap;
+            return map;
         }
 
         List<PredictedObstacle> GetPredictedObstacles(float[] Xs, float[] Ys)
